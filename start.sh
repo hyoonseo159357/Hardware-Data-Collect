75eae707dc79eab1ad1aa54d8e370456489e9506
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2021-10-05 00:41:14 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,8 @@
-pip3 install tensorflow
+pip3 install --upgrade pip
+pip3 install tensorflow-gpu==1.14.0
+pip3 install tensorflow_datasets
+
+python3 sample.py
+python3 sample2.py
+python3 sample3.py
+python3 sample4.py
