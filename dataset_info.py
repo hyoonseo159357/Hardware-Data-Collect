dd21fc1ae5b64895497cbcf626827c4458e161a0
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2021-11-28 15:42:48 +0900

    Update dataset_info.py

--- dataset_info.py
+++ dataset_info.py
@@ -30,8 +30,8 @@ dataset_128 = {
     'img_rows': 128,
     'img_cols': 128,
     'img_channels': 3,
-    'num_data': 7000,
-    'num_test': 1500
+    'num_data': 50000,
+    'num_test': 10000
 }
 
 dataset_224 = {
