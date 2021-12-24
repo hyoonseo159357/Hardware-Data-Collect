6bc8f9663181b42e5360c6c51dae0c21bb36a103
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2021-12-24 13:54:08 +0900

    Create _ResNet50_not_check_epoch.py

---
+++ _ResNet50_not_check_epoch.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+import math
+import time
+import pandas as pd
+import pickle
+import argparse
+import tensorflow as tf
+import numpy as np
+import dataset_info
+import model_info
+import os
+model_name = 'ResNet50'
+datasetsize=128
+batch_size =128 #batch size
+
+epoch_start = 0
+epoch_end = 0
+dataset = dataset_info.select_dataset(datasetsize)
+num_classes = dataset['num_classes']
+img_rows = dataset['img_rows']
+img_cols = dataset['img_cols']
+img_channels = dataset['img_channels']
+num_data = dataset['num_data']
+num_test = dataset['num_test']
+input_data = num_data+num_test
+
+prof_point = 10 #prof_point
+batch_num = math.ceil(num_data/batch_size)
+epochs = math.ceil(prof_point)
+prof_start = math.floor(batch_num * prof_point)
+prof_len = 1 #prof_len
+prof_range = '{}, {}'.format(prof_start, prof_start + prof_len)
+optimizer = 'SGD'
+
+###################### Build Fake Dataset ######################
+x_train_shape = (num_data, img_rows, img_cols, img_channels)
+y_train_shape = (num_data, 1)
+
+x_test_shape = (num_test, img_rows, img_cols, img_channels)
+y_test_shape = (num_test, 1)
+
+x_train = np.random.rand(*x_train_shape)
+y_train = np.random.randint(num_classes, size=y_train_shape)
+x_test = np.random.rand(*x_test_shape)
+y_test = np.random.randint(num_classes, size=y_test_shape)
+###############################################################
+if tf.keras.backend.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], img_channels, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], img_channels, img_rows, img_cols)
+    input_shape = (img_channels, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, img_channels)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, img_channels)
+    input_shape = (img_rows, img_cols, img_channels)
+    
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+y_train = tf.keras.utils.to_categorical(y_train, num_classes)
+y_test = tf.keras.utils.to_categorical(y_test, num_classes)
+
+# Select model from model info module
+model = model_info.select_model(model_name, input_shape, num_classes)
+
+model.compile(loss=tf.keras.losses.categorical_crossentropy,
+              optimizer=optimizer,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+    batch_size=batch_size,
+    epochs=epochs,
+    verbose=1,
+    validation_data=(x_test, y_test),
+    callbacks = [latency_callback])
