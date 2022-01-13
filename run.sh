e311393d9f68459b3522532a6d2e4e9fb319161e
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-01-13 14:08:17 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,3 +4,8 @@ python3 _CIFAR10_CNN.py
 python3 _MNIST_CNN.py
 python3 _InceptionV3.py
 python3 _VGG16.py
+python3 _InceptionResNetV2.py
+python3 _LeNet5.py
+python3 _ResNetSmall.py
+python3 _MobileNetV2.py
+
