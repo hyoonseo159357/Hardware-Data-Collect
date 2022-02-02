32c4b9123cd805bd376333a4db97facf09cfce37
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-02 12:24:24 +0900

    Update start_ec2.sh

--- start_ec2.sh
+++ start_ec2.sh
@@ -24,7 +24,7 @@ sleep 60
 AWS_KEY="ys-oregon2.pem"
 echo 'git clone and setting instance'
 ssh -o "StrictHostKeyChecking no" -i $AWS_KEY ubuntu@$INSTANCE_DNS 'git clone https://github.com/hyoonseo159357/Hardware-Data2.git'
-ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/profet/data_generation/;sudo bash ./settings.sh'
+ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data2/;sudo bash ./settings.sh'
 
 # Run Experiments
 sleep 10
