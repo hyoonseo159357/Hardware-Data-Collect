0fd897eabd976eab5704a59cf87f2f02305f373f
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-02 13:29:04 +0900

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,131 +1,38 @@
 # 'LeNet5'
 python3.6 profile_workload.py --model 'LeNet5' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'LeNet5' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'LeNet5' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'LeNet5' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'LeNet5' --dataset 256 --batch_size 16  
-
 # VGGSmall
 python3.6 profile_workload.py --model 'VGGSmall' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'VGGSmall' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'VGGSmall' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'VGGSmall' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'VGGSmall' --dataset 256 --batch_size 16  
-
 # VGG11
 python3.6 profile_workload.py --model 'VGG11' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'VGG11' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG11' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG11' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG11' --dataset 256 --batch_size 16  
-
 # VGG13
 python3.6 profile_workload.py --model 'VGG13' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'VGG13' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG13' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG13' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG13' --dataset 256 --batch_size 16  
-
 # VGG16
 python3.6 profile_workload.py --model 'VGG16' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'VGG16' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG16' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG16' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG16' --dataset 256 --batch_size 16  
-
 # VGG19
 python3.6 profile_workload.py --model 'VGG16' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'VGG16' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG16' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG16' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'VGG16' --dataset 256 --batch_size 16  
-
 # ResNetSmall
 python3.6 profile_workload.py --model 'ResNetSmall' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'ResNetSmall' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNetSmall' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNetSmall' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNetSmall' --dataset 256 --batch_size 16  
-
 # ResNet18
 python3.6 profile_workload.py --model 'ResNet18' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'ResNet18' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet18' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet18' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet18' --dataset 256 --batch_size 16   
-
 # ResNet34
 python3.6 profile_workload.py --model 'ResNet34' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'ResNet34' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet34' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet34' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet34' --dataset 256 --batch_size 16           
-
 # MNIST_CNN
 python3.6 profile_workload.py --model 'MNIST_CNN' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'MNIST_CNN' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'MNIST_CNN' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'MNIST_CNN' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'MNIST_CNN' --dataset 256 --batch_size 16        
-  
 # CIFAR10_CNN
 python3.6 profile_workload.py --model 'CIFAR10_CNN' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'CIFAR10_CNN' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'CIFAR10_CNN' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'CIFAR10_CNN' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'CIFAR10_CNN' --dataset 256 --batch_size 16  
-
 # FLOWER_CNN
 python3.6 profile_workload.py --model 'FLOWER_CNN' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'FLOWER_CNN' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'FLOWER_CNN' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'FLOWER_CNN' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'FLOWER_CNN' --dataset 256 --batch_size 16  
-
 # AlexNet
 python3.6 profile_workload.py --model 'AlexNet' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'AlexNet' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'AlexNet' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'AlexNet' --dataset 224 --batch_size 16  
-
 # InceptionV3
 python3.6 profile_workload.py --model 'InceptionV3' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'InceptionV3' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'InceptionV3' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'InceptionV3' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'InceptionV3' --dataset 256 --batch_size 16  
-
 # InceptionResNetV2
 python3.6 profile_workload.py --model 'InceptionResNetV2' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'InceptionResNetV2' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'InceptionResNetV2' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'InceptionResNetV2' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'InceptionResNetV2' --dataset 256 --batch_size 16  
-
 # Xception
 python3.6 profile_workload.py --model 'Xception' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'Xception' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'Xception' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'Xception' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'Xception' --dataset 256 --batch_size 16            
-
 # EfficientNetB0
 python3.6 profile_workload.py --model 'EfficientNetB0' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'EfficientNetB0' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'EfficientNetB0' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'EfficientNetB0' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'EfficientNetB0' --dataset 256 --batch_size 16  
-
 # MobileNetV2
 python3.6 profile_workload.py --model 'MobileNetV2' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'MobileNetV2' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'MobileNetV2' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'MobileNetV2' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'MobileNetV2' --dataset 256 --batch_size 16  
-
 # ResNet50
-python3.6 profile_workload.py --model 'ResNet50' --dataset 32 --batch_size 16    
-python3.6 profile_workload.py --model 'ResNet50' --dataset 64 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet50' --dataset 128 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet50' --dataset 224 --batch_size 16  
-python3.6 profile_workload.py --model 'ResNet50' --dataset 256 --batch_size 16           
+python3.6 profile_workload.py --model 'ResNet50' --dataset 32--batch_size 16    
