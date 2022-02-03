38818bb2f42ef6cfccef34ab62dd30e03199d195
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-03 11:06:37 +0900

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 tmux new -s dstattmux
-sudo dstat --output ~/Hardware-Data/dstat-log.csv -cdnpmrt
+sudo dstat --output ~/Hardware-Data2/dstat-log.csv -cdnpmrt
 tmux detach
 tmux new -s nvidiasmitmux
 nvidia-smi -lms 110 -f ./Data.csv --format=csv --query-gpu=timestamp,temperature.gpu,utilization.memory,utilization.gpu,power.draw,clocks.current.sm
