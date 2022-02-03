4e13f06623aa58f9fa6a536b1d34f4f31350d5e0
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-03 11:16:14 +0900

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,3 @@
-tmux new -s dstattmux
-sudo dstat --output ~/Hardware-Data2/dstat-log.csv -cdnpmrt
-tmux detach
-tmux new -s nvidiasmitmux
-nvidia-smi -lms 110 -f ./Data.csv --format=csv --query-gpu=timestamp,temperature.gpu,utilization.memory,utilization.gpu,power.draw,clocks.current.sm
-tmux detach
-tmux new -s maintmux
 
 
 # 'LeNet5'
