2424be4e2a67b2d6ae70ca110514dd49a9ded117
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-03 14:24:26 +0900

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
 chmod +x run_dstat.sh
 chmod +x run_nvidia.sh
 chmod +x run_workload.sh
-nohup ./run_dstat.sh >nohup.out & nohup ./run_nvidia.sh >nohup2.out & nohup ./run_nvidia.sh >nohup3.out & 
+nohup ./run_dstat.sh >nohup.out & nohup ./run_nvidia.sh >nohup2.out & nohup ./run_workload.sh >nohup3.out & 
