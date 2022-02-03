8f18439b5da65ad3b90554d9193e0988bdf7db98
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-03 14:49:46 +0900

    Update startCLI.sh

--- startCLI.sh
+++ startCLI.sh
@@ -23,7 +23,6 @@ AWS_KEY="ys.pem"
 echo 'git clone and setting instance'
 ssh -o "StrictHostKeyChecking no" -i $AWS_KEY ubuntu@$INSTANCE_DNS 'git clone https://github.com/hyoonseo159357/Hardware-Data2.git'
 ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data2/;sudo bash ./settings.sh'
-ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data2/;sudo bash ./settings.sh'
 
 # Run Experiments
 sleep 10
