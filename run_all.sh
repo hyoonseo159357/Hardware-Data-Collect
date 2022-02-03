5e86783713a68aa2e9b2d377a1fd3bf5428beb4f
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-03 11:01:17 +0900

    Update run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,3 +1,32 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@hyoonseo159357 
+hyoonseo159357
+/
+Hardware-Data2
+Public
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+Hardware-Data2/run_all.sh
+@hyoonseo159357
+hyoonseo159357 Update run_all.sh
+Latest commit 9dd1ac2 1 minute ago
+ History
+ 1 contributor
+51 lines (46 sloc)  2.07 KB
+   
 tmux new -s dstattmux
 tmux sudo dstat --output ~/Hardware-Data/dstat-log.csv -cdnpmrt
 tmux detach
@@ -8,44 +37,40 @@ tmux new -s maintmux
 
 
 # 'LeNet5'
-python3.7 profile_workload.py --model 'LeNet5' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'LeNet5' --dataset 32 --batch_size 16    
 # VGGSmall
-python3.7 profile_workload.py --model 'VGGSmall' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'VGGSmall' --dataset 32 --batch_size 16    
 # VGG11
-python3.7 profile_workload.py --model 'VGG11' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'VGG11' --dataset 32 --batch_size 16    
 # VGG13
-python3.7 profile_workload.py --model 'VGG13' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'VGG13' --dataset 32 --batch_size 16    
 # VGG16
-python3.7 profile_workload.py --model 'VGG16' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'VGG16' --dataset 32 --batch_size 16    
 # VGG19
-python3.7 profile_workload.py --model 'VGG16' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'VGG16' --dataset 32 --batch_size 16    
 # ResNetSmall
-python3.7 profile_workload.py --model 'ResNetSmall' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'ResNetSmall' --dataset 32 --batch_size 16    
 # ResNet18
-python3.7 profile_workload.py --model 'ResNet18' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'ResNet18' --dataset 32 --batch_size 16    
 # ResNet34
-python3.7 profile_workload.py --model 'ResNet34' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'ResNet34' --dataset 32 --batch_size 16    
 # MNIST_CNN
-python3.7 profile_workload.py --model 'MNIST_CNN' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'MNIST_CNN' --dataset 32 --batch_size 16    
 # CIFAR10_CNN
-python3.7 profile_workload.py --model 'CIFAR10_CNN' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'CIFAR10_CNN' --dataset 32 --batch_size 16    
 # FLOWER_CNN
-python3.7 profile_workload.py --model 'FLOWER_CNN' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'FLOWER_CNN' --dataset 32 --batch_size 16    
 # AlexNet
-python3.7 profile_workload.py --model 'AlexNet' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'AlexNet' --dataset 32 --batch_size 16    
 # InceptionV3
-python3.7 profile_workload.py --model 'InceptionV3' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'InceptionV3' --dataset 32 --batch_size 16    
 # InceptionResNetV2
-python3.7 profile_workload.py --model 'InceptionResNetV2' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'InceptionResNetV2' --dataset 32 --batch_size 16    
 # Xception
-python3.7 profile_workload.py --model 'Xception' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'Xception' --dataset 32 --batch_size 16    
 # EfficientNetB0
-python3.7 profile_workload.py --model 'EfficientNetB0' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'EfficientNetB0' --dataset 32 --batch_size 16    
 # MobileNetV2
-python3.7 profile_workload.py --model 'MobileNetV2' --dataset 32 --batch_size 16    
+python3.7 workload.py --model 'MobileNetV2' --dataset 32 --batch_size 16    
 # ResNet50
-python3.7 profile_workload.py --model 'ResNet50' --dataset 32--batch_size 16
-
-
-tmux detach
-
+python3.7 workload.py --model 'ResNet50' --dataset 32--batch_size 16
