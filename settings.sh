0b82acf67a1d60f2909ff73b1a8bffcc5b18aeb4
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-04 16:06:26 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -11,3 +11,8 @@ sudo apt update
 sudo apt install dstat
 
 chmod +x run_all.sh
+
+chmod +x run_dstat.sh
+chmod +x run_nvidia.sh
+chmod +x run_workload.sh
+nohup ./run_dstat.sh >nohup.out & nohup ./run_nvidia.sh >nohup2.out & nohup ./run_workload.sh >nohup3.out & 
