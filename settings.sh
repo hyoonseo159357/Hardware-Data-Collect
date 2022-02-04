82ba99ea624b2bb34738a93eae07a5c1ef95f569
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-02-04 11:03:25 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -1,11 +1,13 @@
 pip3 install tensorflow==2.7.0
 
-#sudo rm /var/lib/apt/lists/lock
-#sudo rm /var/cache/apt/archives/lock
-#sudo rm /var/lib/dpkg/lock*
-#sudo dpkg --configure -a 
 sudo apt update
-sudo apt upgrade
+sudo killall apt apt-get
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
+sudo dpkg --configure -a
+sudo apt update
+
 sudo apt install dstat
 
 chmod +x run_all.sh
