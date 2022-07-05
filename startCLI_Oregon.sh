f139b6afcb2ea12bb0afdde403eb9f8734d9e21f
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-05 23:12:59 +0900

    Update startCLI_Oregon.sh

--- startCLI_Oregon.sh
+++ startCLI_Oregon.sh
@@ -19,14 +19,14 @@ echo $INSTANCE_DNS
 
 # Instance setting
 sleep 60
-AWS_KEY="ys.pem"
+AWS_KEY="ys-oregon2"
 echo 'git clone and setting instance'
 ssh -o "StrictHostKeyChecking no" -i $AWS_KEY ubuntu@$INSTANCE_DNS 'git clone https://github.com/hyoonseo159357/Hardware-Data-Collect.git'
 ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./settings.sh && sudo bash ./run_all.sh'
 
 # # Run Experiments
 # sleep 60
-# AWS_KEY="ys.pem"
+# AWS_KEY="ys-oregon2"
 # echo 'start experiment'
 # ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./run_all.sh'
 #ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 
