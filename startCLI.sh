675bfbac0ff4cd674e4a61c43464d71fc0f6bcc5
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-05 17:33:29 +0900

    Update startCLI.sh

--- startCLI.sh
+++ startCLI.sh
@@ -22,13 +22,13 @@ sleep 60
 AWS_KEY="ys.pem"
 echo 'git clone and setting instance'
 ssh -o "StrictHostKeyChecking no" -i $AWS_KEY ubuntu@$INSTANCE_DNS 'git clone https://github.com/hyoonseo159357/Hardware-Data-Collect.git'
-ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./settings.sh'
+ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./settings.sh && sudo bash ./run_all.sh'
 
-# Run Experiments
-sleep 60
-AWS_KEY="ys.pem"
-echo 'start experiment'
-ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./run_all.sh'
+# # Run Experiments
+# sleep 60
+# AWS_KEY="ys.pem"
+# echo 'start experiment'
+# ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./run_all.sh'
 #ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 
 
 # # Get results
