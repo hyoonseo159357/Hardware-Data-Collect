fef71fcb5de7cb6dd593a8e0a9e75e8642b8cf69
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-06 11:05:21 +0900

    Update Run_nvidiasmi_dstat.sh

--- Run_nvidiasmi_dstat.sh
+++ Run_nvidiasmi_dstat.sh
@@ -1,4 +1,4 @@
-chmod +x run_dstat.sh
-chmod +x run_nvidia.sh
-chmod +x run_workload.sh
-nohup ./run_dstat.sh >nohup.out & nohup ./run_nvidia.sh >nohup2.out & nohup ./run_workload.sh >nohup3.out & 
+chmod +x dstat.sh
+chmod +x nvidiasmi.sh
+chmod +x allworkload.sh
+nohup ./dstat.sh >nohup.out & nohup ./nvidiasmi.sh >nohup2.out & nohup ./allworkload.sh >nohup3.out & 
